ef5a4a654efe03a91740d2ae6f5bcc8d70f1d1eb
Lucioo04 <luciotesta@gmail.com>
2023-10-18 10:34:07 -0300

    Agregue el archivo app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000,()=>{
+    console.log("Servicio ubicados en el puerto 3000");
+})
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/home.html'));
+})

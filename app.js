50c39155a78a46bc4d12caf6af6734cfa45391af
Lucioo04 <luciotesta@gmail.com>
2023-11-09 20:54:40 -0300

    agregue login.html y register.html con sus hojas de estilos correspondientes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(3000,()=>{
     console.log("Servicio ubicados en el puerto 3000");
@@ -8,6 +11,23 @@ app.listen(3000,()=>{
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'));
+})
+
+app.get('/registro',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'));
+})
+
+app.post('/registro',(req,res)=>{
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.post ('/registro',(req,res)=>{
+console.log(req.body);
+res.redirect("/");
 })
 
 app.use(express.static(path.join(__dirname, '/public')));
